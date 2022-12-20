b10a58c632ef10f955beede52e59878345edf79c
AnithaPriyanka <119839914+AnithaPriyanka@users.noreply.github.com>
2022-12-20 21:58:49 +0530

    #include<stdio.h> int main() {     int n,k,m,bags;     scanf("%d%d%d",&n,&k,&m);     bags=k*m;     if(n%bags==0)     {         printf("%d",n/bags);     }     else     {         printf("%d",n/bags+1);     } }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,m;
+    int n,k,m,bags;
     scanf("%d%d%d",&n,&k,&m);
-    int cpb=k*m;
-    if(n%cpb==0) printf("%d",n/cpb);
-    else printf("%d",n/cpb+1);
+    bags=k*m;
+    if(n%bags==0)
+    {
+        printf("%d",n/bags);
+    }
+    else
+    {
+        printf("%d",n/bags+1);
+    }
 }
