46c9e02e2e649551c3e79213366faf876ed1c2d5
AnithaPriyanka <119839914+AnithaPriyanka@users.noreply.github.com>
2023-01-15 21:50:28 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(j=1;j<=n;j++)         {             if(i==1||j==1||i==n||j==n)             printf("*");             else             printf(" ");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,12 +5,16 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
         for(j=1;j<=n;j++)
         {
-            if(i==j)
-            printf("0");
+            if(i==1||j==1||i==n||j==n)
+            printf("*");
             else
-            printf("x");
+            printf(" ");
         }
         printf("
 ");
