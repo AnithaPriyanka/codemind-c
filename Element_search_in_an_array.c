de1ab9f712f150c9533bd3f62ad034ca082b2f36
AnithaPriyanka <119839914+AnithaPriyanka@users.noreply.github.com>
2023-05-16 22:30:20 +0530

    #include<stdio.h> int linearsearch(int *arr,int n,int i,int k) {     if(i<n)     {         if(arr[i]==k)             return i;         else             return linearsearch(arr,n,i+1,k);     }     return -1; } int main() {     int n,c=0,f;     scanf("%d",&n);     int i,arr[n],k;     for(i=0;i<n;i++)         scanf("%d",&arr[i]);         scanf("%d",&k);         f=linearsearch(arr,n,0,k);     if(f==-1)         printf("False");     else         printf("True"); }

---
+++ Element_search_in_an_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int linearsearch(int *arr,int n,int i,int k)
+{
+    if(i<n)
+    {
+        if(arr[i]==k)
+            return i;
+        else
+            return linearsearch(arr,n,i+1,k);
+    }
+    return -1;
+}
+int main()
+{
+    int n,c=0,f;
+    scanf("%d",&n);
+    int i,arr[n],k;
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+        scanf("%d",&k);
+        f=linearsearch(arr,n,0,k);
+    if(f==-1)
+        printf("False");
+    else
+        printf("True");
+}
