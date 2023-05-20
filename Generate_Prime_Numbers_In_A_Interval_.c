e72794bd968c0c8400c0ff6756dc266e3c216508
AnithaPriyanka <119839914+AnithaPriyanka@users.noreply.github.com>
2023-05-20 23:58:38 +0530

    #include<stdio.h> int isprime(int num){     if(num<=1){         return 0;     }     for(int i=2;i*i<=num;i++){         if(num%i==0){             return 0;         }     }     return 1; } void primeRange(int st, int en){     int cnt=0;     for(int i=st;i<=en;i++){         if(isprime(i)){             printf("%d ", i);         }     }
    }
    int main(){
        int st, en;
        scanf("%d %d", &st, &en);
        primeRange(st, en);
    }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int isprime(int num){
+    if(num<=1){
+        return 0;
+    }
+    for(int i=2;i*i<=num;i++){
+        if(num%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+void primeRange(int st, int en){
+    int cnt=0;
+    for(int i=st;i<=en;i++){
+        if(isprime(i)){
+            printf("%d
+", i);
+        }
+    }
+
+}
+int main(){
+    int st, en;
+    scanf("%d %d", &st, &en);
+    primeRange(st, en);
+}
