e31ff52f0476badfcc1fc885929a07c95cc81e45
AnithaPriyanka <119839914+AnithaPriyanka@users.noreply.github.com>
2023-11-05 21:18:44 +0530

    #include<stdio.h> int main(){     int n,t,r,s;     scanf("%d",&n);     if(n<0)     printf("%d is not a positive number.",n);     else{         t=n;         while(t>9){             s=0;             while(t!=0){                 r=t%10;                 t=t/10;                 s+=r;             }             t=s;         }         printf("%d",t);     }
    }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
-int Sum(int n){
-    int r, s=0;
-    while(n>0){
-        r=n%10;
-        s=s+r;
-        n=n/10;
-    }
-    return s;
-}
-int SingleDigit(int n){
-    while(n>=10){
-        n=Sum(n);
-    }
-    return n;
-}
 int main(){
-    int n;
-    scanf("%d", &n);
-    int res;
-    res = SingleDigit(n);
-    printf("%d", res);
+    int n,t,r,s;
+    scanf("%d",&n);
+    if(n<0)
+    printf("%d is not a positive number.",n);
+    else{
+        t=n;
+        while(t>9){
+            s=0;
+            while(t!=0){
+                r=t%10;
+                t=t/10;
+                s+=r;
+            }
+            t=s;
+        }
+        printf("%d",t);
+    }
+    
 }
